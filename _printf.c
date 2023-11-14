0ab2fd43d3b8392bedb204f45e3585ac9c062cd7
Lucky <luckybaraka21@gmail.com>
2023-11-14 05:15:02 -0800

    Changes kibao

--- _printf.c
+++ _printf.c
@@ -1,42 +1,34 @@
 #include "main.h"
 
 /**
- * _printf - function that prints char on the console
- * @format: character strings
- * Return: the lenghth of the string
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
 
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match cl[] = {
-		{"%s", print_string}, {"%c", print_char}, {"%%", print_27}
-	};
+	int printed = 0;
 
 	va_list args;
-	int i = 0, ci, len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-ProcessFormat:
-		while (format[i] != '\0')
+	while (*format != '\0')
+	{
+		if (*format == '%')
 		{
-			ci = 13;
-			while (ci >= 0)
-			{
-				if (cl[ci].id[0] == format[i] && cl[ci].id[1] == format[i + 1])
-				{
-					len += cl[ci].f(args);
-					i = i + 2;
-					goto ProcessFormat;
-				}
-				ci--;
-			}
-			_putchar(format[i]);
-			len++;
-			i++;
-			}
-		va_end(args);
-	return (len);
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
 	}
+	va_end(args);
+	return (printed);
+}
--- main.h
+++ main.h
@@ -2,36 +2,21 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
 #include <unistd.h>
+#include <limits.h>
 
-/**
- * struct format - match the conversion specifiers for printf
- * @id: type character pointer of the specifier
- * @f: type pointer to function for the conversion specifer
- *
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
+int _putchar(char character);
+int printf_char(va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
+int selector(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_string(va_list val);
-int print_char(va_list val);
-int print_27(void);
-int _strlenc(const char *s);
-int _strlen(char *s);
-int convert_integer(va_list val);
-int print_unsigned_int(va_list val);
-int print_binary(va_list args);
-int convert_integer(va_list args);
-int print_unsigned_int(va_list args);
-
+int _x(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
 
 #endif
