ff606e52478461c4c8b1b06ee86cf9718f3fa936
Lucky <luckybaraka21@gmail.com>
2023-11-14 08:09:50 -0800

    I love my rubindi

--- main.h
+++ main.h
@@ -12,12 +12,7 @@ int print_char(va_list args, int p);
 int print_string(va_list args, int p);
 int print_int(va_list args, int p);
 int all_cases(const char *format, va_list args, int p);
-/*int printf_binary(unsigned int num, int printed);*/
 int _printf(const char *format, ...);
-/*int _x(unsigned int num, int printed, int uppercase);*/
-/*int printf_octal(unsigned int num, int printed);*/
 int print_unsigned_int(unsigned int number, int p);
-/*int printf_reverse(va_list args, int printed);*/
-/*int printf_pointer(va_list args, int printed);*/
 
 #endif
