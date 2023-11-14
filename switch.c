131a79553d37203586bdd2687c829af063b83070
NyamburaKariuki <prettimercii@gmail.com>
2023-11-14 18:58:20 +0300

    all cases

--- switch.c
+++ switch.c
@@ -27,28 +27,7 @@ int all_cases(const char *format, va_list args, int p)
 			_putchar('%');
 			p++;
 			break;
-		/*
-		*case 'b':
-			*printed = printf_binary(va_arg(args, unsigned int), printed);
-			*break;
-		*case 'x':
-		*case 'X':
-		*	printed = _x(va_arg(args, unsigned int), printed, (*format == 'X') ? 1 : 0);
-		*	break;
-		*case 'o':
-		*	printed = printf_octal(va_arg(args, unsigned int), printed);
-		*	break;
-		*case 'u':
-		*	printed = printf_unsigned(va_arg(args, unsigned int), printed);
-		*	break;
-		*case 'r':
-		*	printed = printf_reverse(args, printed);
-		*	break;
-		*case 'p':
-		*	printed = printf_pointer(args, printed);
-		*	break;
-		*/
-		default:
+			default:
 			break;
 	}
 	return (p);
